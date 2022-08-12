762d7ee19f8e6c4dd4b0de1ebc325316eed91d54
hillaliy <hillaliy@me.com>
2022-08-12 20:20:10 +0300

    Update MideaPlatform.js

--- dist/MideaPlatform.js
+++ dist/MideaPlatform.js
@@ -15,29 +15,28 @@ class MideaPlatform {
         this.mideaAccessories = [];
         this.updateInterval = null;
         this.refreshTimeout = null;
-        // super({
-        //   ...Options,
-        //   name: "midea",
-        // });
+        this.devices = [];
+        this.accessToken = '';
+        this.key = '';
         this.log.debug('Finished initializing platform:', this.config.name);
         this.log = log;
         this.config = config;
         this.appCredentials = {
-            nethome: {
+            NetHomePlus: {
                 appkey: "3742e9e5842d4ad59c2db887e12449f9",
                 appid: 1017,
                 api_url: "https://mapp.appsmb.com",
                 sign_key: "xhdiwjnchekd4d512chdjx5d8e4c394D2D7S",
                 proxied: null,
             },
-            midea: {
+            MideaAir: {
                 appkey: "ff0cf6f5f0c3471de36341cab3f7a9af",
                 appid: 1117,
                 api_url: "https://mapp.appsmb.com",
                 sign_key: "xhdiwjnchekd4d512chdjx5d8e4c394D2D7S",
                 proxied: null,
             },
-            msmarthome: {
+            MSmartHome: {
                 appkey: "ac21b9f9cbfe4ca5a88562ef25e2b768",
                 appid: 1010,
                 api_url: "https://mp-prod.appsmb.com/mas/v5/app/proxy?alias=",
@@ -50,15 +49,10 @@ class MideaPlatform {
         api.on('didFinishLaunching', () => {
             this.onReady();
             // this.onUnload();
-            // this.on('ready', this.onReady.bind(this));
-            // this.on("stateChange", this.onStateChange.bind(this));
-            // this.on('unload', this.onUnload.bind(this));
-            // this.json2iob = new Json2iob(this);
+            // this.onStateChange;
             this.log.debug('Executed didFinishLaunching callback');
-            // this.devices = {};
         });
     }
-    // Is called when databases are connected and adapter received configuration.
     async onReady() {
         try {
             this.midea_beautiful = await python('midea_beautiful');
@@ -78,21 +72,6 @@ class MideaPlatform {
         catch (error) {
             this.log.debug('Load "midea_beautiful" failed');
         }
-        // this.setState("info.connection", false, true);
-        // if (this.config.interval < 0.5) {
-        //   this.log.info("Set interval to minimum 0.5");
-        //   this.config.interval = 0.5;
-        // }
-        // if (!this.config.user || !this.config.password) {
-        //   this.log.error(
-        //     "Please set username and password in the instance settings"
-        //   );
-        //   return;
-        // }
-        // Reset the connection indicator during startup
-        // this.setState("info.connection", false, true);
-        // in this template all states changes inside the adapters namespace are subscribed
-        // this.subscribeStates("*");
     }
     configureAccessory(accessory) {
         this.log.info(`Loading accessory from cache: ${accessory.displayName}`);
@@ -104,9 +83,14 @@ class MideaPlatform {
             const cloud = await this.midea_beautiful.connect_to_cloud$({
                 account: this.config['user'],
                 password: this.config['password'],
-                ...this.appCredentials.msmarthome, //[this.config.supportedApps],
+                ...this.appCredentials[this.config['appCredentials']],
             });
-            this.log.debug(await cloud.__dict__);
+            const cloudDict = await cloud.__dict__;
+            this.log.debug(cloudDict);
+            this.accessToken = cloudDict.accessToken;
+            console.log(this.accessToken);
+            // this.key = cloudDict.key;
+            // console.log(this.key)
             this.log.info('Login successful');
             // this.setState("info.connection", true, true);
             return cloud;
@@ -122,46 +106,48 @@ class MideaPlatform {
                 cloud: this.cloud,
             });
             this.log.debug(`Found ${await this.appliances.length} devices`);
-            // this.log.debug(this.appliances);
-            // this.log.info(this.appliances[0]);
             for await (const [index, app] of await py.enumerate(this.appliances)) {
                 this.log.debug(await app);
                 const appJsonString = this.pythonToJson(await app.state.__dict__.__str__());
                 const appJson = JSON.parse(appJsonString);
-                this.log.debug(appJson);
                 const id = appJson.id;
                 this.devices[id] = appJson;
-                for (const device of this.devices) {
-                    const uuid = this.api.hap.uuid.generate(device.UniqueId);
-                    const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
-                    if (existingAccessory) {
-                        this.log.debug(`Restoring existing accessory from cache: ${existingAccessory.displayName}`);
-                        existingAccessory.context.deviceId = device.id;
-                        existingAccessory.context.name = device.displayName;
-                        this.api.updatePlatformAccessories([existingAccessory]);
-                        const ma = new MideaAccessory_1.MideaAccessory(this, existingAccessory, device.id, parseInt(device.type), device.displayName, device.userId);
-                        this.mideaAccessories.push(ma);
-                        this.configureAccessory(existingAccessory);
-                    }
-                    else {
-                        this.log.debug(`Adding new device: ${device.displayName}`);
-                        const accessory = new this.api.platformAccessory(device.displayName, uuid);
-                        accessory.context.deviceId = device.id;
-                        accessory.context.name = device.displayName;
-                        const ma = new MideaAccessory_1.MideaAccessory(this, accessory, device.id, parseInt(device.type), device.displayName, device.userId);
-                        this.api.registerPlatformAccessories(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, [accessory]);
-                        this.mideaAccessories.push(ma);
-                        this.configureAccessory(accessory);
-                    }
+                let deviceType;
+                if (appJson.type === 'ac') {
+                    deviceType = 172;
                 }
-                // this.setObjectNotExistsAsync(id, {
-                //   type: 'device',
-                //   common: {
-                //     name: appJson.name,
-                //   },
-                //   native: {},
-                // });
-                // this.json2iob.parse(id, appJson, { write: true, forceIndex: true });
+                else if (appJson.type === 'dh') {
+                    deviceType = 161;
+                }
+                else {
+                    this.log.warn(`Device: ${appJson.name} is of unsupported type: ${appJson.type}`);
+                    this.log.warn('Please open an issue on GitHub with your specific device model');
+                }
+                const uuid = this.api.hap.uuid.generate(appJson.id);
+                const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+                if (existingAccessory) {
+                    this.log.debug(`Restoring existing accessory from cache: ${existingAccessory.displayName}`);
+                    existingAccessory.context.deviceId = appJson.id;
+                    existingAccessory.context.name = appJson.name;
+                    existingAccessory.context.type = deviceType;
+                    existingAccessory.context.address = appJson.address;
+                    this.api.updatePlatformAccessories([existingAccessory]);
+                    const ma = new MideaAccessory_1.MideaAccessory(this, existingAccessory, appJson.id, deviceType, appJson.name, appJson.address);
+                    this.mideaAccessories.push(ma);
+                    this.configureAccessory(existingAccessory);
+                }
+                else {
+                    this.log.debug(`Adding new device: ${appJson.name}`);
+                    const accessory = new this.api.platformAccessory(appJson.name, uuid);
+                    accessory.context.deviceId = appJson.id;
+                    accessory.context.name = appJson.name;
+                    accessory.context.type = deviceType;
+                    accessory.context.address = appJson.address;
+                    const ma = new MideaAccessory_1.MideaAccessory(this, accessory, appJson.id, deviceType, appJson.name, appJson.address);
+                    this.api.registerPlatformAccessories(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, [accessory]);
+                    this.mideaAccessories.push(ma);
+                    this.configureAccessory(accessory);
+                }
             }
         }
         catch (error) {
@@ -170,43 +156,147 @@ class MideaPlatform {
     }
     async updateDevices() {
         try {
-            for (const id in this.devices) {
-                let applianceState;
-                if (this.config.local) {
-                    if (this.devices[id].address) {
-                        applianceState = await this.midea_beautiful.applianceState$({
-                            address: this.devices[id].address,
-                            token: this.devices[id].token,
-                            key: this.devices[id].key,
-                            applianceId: id,
-                        });
-                    }
-                    else {
-                        this.log.info(`Missing ip for ${id} device update via cloud`);
-                        applianceState = await this.midea_beautiful.applianceState$({
-                            cloud: this.cloud,
-                            applianceId: id,
-                            useCloud: true,
-                        });
-                    }
+            this.accessories.forEach(async (accessory) => {
+                this.log.debug(`Updating accessory: ${accessory.context.name} (${accessory.context.deviceId})`);
+                let applianceType;
+                if (accessory.context.type === 172) {
+                    applianceType = 'ac';
                 }
-                else {
-                    applianceState = await this.midea_beautiful.applianceState$({
-                        cloud: this.cloud,
-                        applianceId: id,
-                        useCloud: true,
+                else if (accessory.context.type === 161) {
+                    applianceType = 'dh';
+                }
+                let applianceState;
+                if (accessory.context.address) {
+                    this.log.debug(`Update device ${accessory.context.name} (${accessory.context.deviceId}) via lan`);
+                    applianceState = await this.midea_beautiful.appliance_state$({
+                        address: accessory.context.address,
+                        token: accessory.context.token,
+                        key: accessory.context.key,
+                        appliance_id: accessory.context.deviceId,
                     });
                 }
+                else {
+                    this.log.debug(`Update device ${accessory.context.name} (${accessory.context.deviceId}) via cloud`);
+                    applianceState = await this.midea_beautiful.appliance_state$({
+                        cloud: this.cloud,
+                        use_cloud: true,
+                        appliance_id: accessory.context.deviceId,
+                        appliance_type: applianceType,
+                    });
+                }
+                ;
                 this.log.debug(await applianceState);
                 const stateString = this.pythonToJson(await applianceState.state.__dict__.__str__());
                 const stateJson = JSON.parse(stateString);
-                // this.json2iob.parse(id, stateJson, { write: true, forceIndex: true });
-            }
+                let mideaAccessory = this.mideaAccessories.find(ma => ma.deviceId === accessory.context.deviceId);
+                mideaAccessory.powerState = stateJson.running ? 1 : 0;
+                mideaAccessory.beepPrompt = stateJson.beep_prompt;
+                mideaAccessory.operationalMode = stateJson.mode;
+                mideaAccessory.fanSpeed = stateJson.fan_speed;
+                mideaAccessory.verticalSwing = stateJson.vertical_swing;
+                mideaAccessory.horizontalSwing = stateJson.horizontal_swing;
+                if (accessory.context.type === 172) {
+                    mideaAccessory.targetTemperature = stateJson.target_temperature;
+                    mideaAccessory.indoorTemperature = stateJson.indoor_temperature;
+                    mideaAccessory.outdoorTemperature = stateJson.outdoor_temperature;
+                    mideaAccessory.useFahrenheit = stateJson.fahrenheit;
+                    mideaAccessory.turboFan = stateJson.turbo_fan;
+                    mideaAccessory.ecoMode = stateJson.eco_mode;
+                    mideaAccessory.turboMode = stateJson.turbo;
+                    mideaAccessory.purifier = stateJson.purifier;
+                    mideaAccessory.dryer = stateJson.dryer;
+                    mideaAccessory.comfortSleep = stateJson.comfort_sleep;
+                    mideaAccessory.showScreen = stateJson.show_screen;
+                    this.log.debug(`\nPower state: ${mideaAccessory.powerState}\nBeep prompt: ${mideaAccessory.beepPrompt}\nOperational mode: ${mideaAccessory.operationalMode}\nFan speed: ${mideaAccessory.fanSpeed}\nTarget temperature: ${mideaAccessory.targetTemperature}\nIndoor temperature: ${mideaAccessory.indoorTemperature}\nOutdoor temperature: ${mideaAccessory.outdoorTemperature}\nVertical swing: ${mideaAccessory.verticalSwing}\nHorizontal swing: ${mideaAccessory.horizontalSwing}\nFahrenheit: ${mideaAccessory.useFahrenheit}\nturbo fan: ${mideaAccessory.turboFan}\nEco mode: ${mideaAccessory.ecoMode}\nTurbo mode: ${mideaAccessory.turboMode}\nPurifier: ${mideaAccessory.purifier}\nDryer: ${mideaAccessory.dryer}\nComfort sleep: ${mideaAccessory.comfortSleep}\nShow screen: ${mideaAccessory.showScreen}`);
+                }
+                else if (accessory.context.type === 161) {
+                    mideaAccessory.currentHumidity = stateJson.current_humidity;
+                    mideaAccessory.targetHumidity = stateJson.target_humidity;
+                    mideaAccessory.tankLevel = stateJson.tank_level;
+                    this.log.debug(`\nPower state: ${mideaAccessory.powerState}\nBeep prompt: ${mideaAccessory.beepPrompt}\nOperational mode: ${mideaAccessory.operationalMode}\nFan speed: ${mideaAccessory.fanSpeed}\nCurrent humidity: ${mideaAccessory.currentHumidity}\nTarget humidity: ${mideaAccessory.targetHumidity}\nTank level: ${mideaAccessory.tankLevel}\nVertical swing: ${mideaAccessory.verticalSwing}\nHorizontal swing: ${mideaAccessory.horizontalSwing}`);
+                }
+            });
         }
         catch (error) {
             this.log.error(error);
         }
     }
+    async sendUpdateToDevice(id, device) {
+        if (device) {
+            // const deviceId = id.split(".")[2];
+            // console.log(deviceId)
+            const command = id.split(".").pop;
+            const index = Object.keys(this.devices).indexOf(id);
+            const appliance = await this.appliances[index];
+            const setState = { cloud: this.cloud };
+            let cmd = [];
+            cmd.running = device.powerState;
+            cmd.beep_prompt = device.beepPrompt;
+            cmd.mode = device.operationalMode;
+            cmd.fan_speed = device.fanSpeed;
+            cmd.vertical_swing = device.verticalSwing;
+            cmd.horizontal_swing = device.horizontalSwing;
+            if (device.deviceType === 172) {
+                cmd.target_temperature = device.targetTemperature;
+                cmd.fahrenheit = device.useFahrenheit;
+                cmd.turbo_fan = device.turboFan;
+                cmd.eco_mode = device.ecoMode;
+                cmd.turbo = device.turboMode;
+                cmd.purifier = device.purifier;
+                cmd.dryer = device.dryer;
+                cmd.comfort_sleep = device.comfortSleep;
+                cmd.show_screen = device.showScreen;
+            }
+            else if (device.deviceType === 161) {
+                cmd.current_humidity = device.currentHumidity;
+                cmd.target_humidity = device.targetHumidity;
+                cmd.tank_level = device.tankLevel;
+            }
+            setState[command] = cmd;
+            this.log.debug(`Send command to: ${device.name} (${device.deviceId})\n${cmd}`);
+            this.log.debug(JSON.stringify(setState));
+            try {
+                await appliance.set_state$(setState);
+            }
+            catch (error) {
+                this.log.error(error);
+            }
+        }
+        ;
+    }
+    ;
+    // onUnload() {
+    //   try {
+    //     process.exit();
+    //     // this.updateInterval && clearInterval(this.updateInterval);
+    //     // this.refreshTimeout && clearTimeout(this.refreshTimeout);
+    //     this.log.debug('Process exit')
+    //   } catch (error: any) {
+    //     this.log.error(error);
+    //   }
+    // }
+    //   async onStateChange(id: string, state: ioBroker.State | null | undefined) {
+    //     if (state && !state.ack) {
+    //       const deviceId = id.split(".")[2];
+    //       const command = id.split(".").pop;
+    //       const index = Object.keys(this.devices).indexOf(deviceId);
+    //       const appliance = await this.appliances[index];
+    //       const setState = { cloud: this.cloud };
+    //       setState[command] = state.val;
+    //       this.log.debug(JSON.stringify(setState));
+    //       try {
+    //         await appliance.set_state$(setState);
+    //       } catch (error) {
+    //         this.log.error(error);
+    //         error.stack && this.log.error(error.stack);
+    //       }
+    //       clearTimeout(this.refreshTimeout);
+    //       this.refreshTimeout = setTimeout(async () => {
+    //         await this.updateDevices();
+    //       }, 10 * 1000);
+    //     }
+    //   }
+    // }
     pythonToJson(objectString) {
         objectString = objectString
             .replaceAll(/b'[^']*'/g, '\'\'')
@@ -220,17 +310,25 @@ class MideaPlatform {
         this.log.debug(objectString);
         return objectString;
     }
-    onUnload() {
-        try {
-            process.exit();
-            // this.updateInterval && clearInterval(this.updateInterval);
-            // this.refreshTimeout && clearTimeout(this.refreshTimeout);
-            // callback();
+    ;
+    getDeviceSpecificOverrideValue(deviceId, key) {
+        if (this.config) {
+            if (this.config.hasOwnProperty('devices')) {
+                for (let i = 0; i < this.config.devices.length; i++) {
+                    if (this.config.devices[i].deviceId === deviceId) {
+                        return this.config.devices[i][key];
+                    }
+                    ;
+                }
+                ;
+            }
+            ;
         }
-        catch (error) {
-            // callback();
-        }
+        ;
+        return null;
     }
+    ;
 }
 exports.MideaPlatform = MideaPlatform;
+;
 //# sourceMappingURL=MideaPlatform.js.map
